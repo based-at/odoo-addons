b50d12720f5b82b798886101156c01c0c1e3996d
Vadim <vadim@based.at>
2016-06-12 14:41:12 +0200

    web_* modules

---
+++ web_highlight/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Web UI: Highlight Row',
+    'category': 'Hidden',
+    'author': 'Vadim',
+    'description': """
+Web UI: Highlight Row
+=====================
+Highlights table row in one2many list view when clicked.
+""",
+    'version': '0.1',
+    'depends': ['base'],
+    "data": [
+        'views/templates.xml'
+    ]
+}
---
+++ web_returnkey/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Web UI: "Return" Key',
+    'category': 'Hidden',
+    'author': 'Vadim',
+    'description': """
+Web UI: "Return" Key
+====================
+Changes "Enter" key behaviour in UI form: jump to next input field instead of doing nothing.
+""",
+    'version': '0.1',
+    'depends': ['base'],
+    "data": [
+        'views/templates.xml'
+    ]
+}
